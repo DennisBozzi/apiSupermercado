47e337e9586a6cd209cc28d2dd82be65736a54fb
Bruno Braga <brunobragatavares94@gmail.com>
2026-05-26 22:20:24 -0300

    adicionando entidade produto

---
+++ src/Domain/Entities/Produto.cs
@@ -0,0 +1,9 @@
+namespace ApiSupermercado.Domain.Entities;
+
+public class Produto
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public int Quantidade { get; set; }
+    public decimal Preco { get; set; }
+}
